2cf5ae2e20bcafb70e8563b2e99ae760586d2124
agustin.gonzalez <agustin.gonzalez@rappi.com>
2024-03-26 20:55:25 -0300

    Adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AboutMeComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  //  { path: 'projects', component: ProjectsComponent },
+  //  { path: 'contact', component: ContactComponent },
+  //  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
