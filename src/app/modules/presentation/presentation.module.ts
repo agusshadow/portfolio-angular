b39b1dddb241a5efc3805f90cfcdb5773ed96919
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2024-03-27 20:06:21 -0300

    Adding about-me component

--- src/app/modules/presentation/presentation.module.ts
+++ src/app/modules/presentation/presentation.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PresentationComponent } from './components/presentation/presentation.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 import { TranslocoRootModule } from 'src/app/transloco-root.module';
 
 @NgModule({
   declarations: [
-    PresentationComponent
+    PresentationComponent,
+    AboutMeComponent
   ],
   imports: [
     CommonModule,
     TranslocoRootModule
   ],
   exports: [
-    PresentationComponent
+    PresentationComponent,
+    AboutMeComponent
   ]
 })
 export class PresentationModule { }
