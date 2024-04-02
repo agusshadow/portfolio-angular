37b3e721f184c772473711fc33a2e79fdeea6ee9
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2024-04-02 19:28:38 -0300

    Adding param -o to ng serve in package

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PresentationComponent } from './modules/presentation/components/presentation/presentation.component';
+import { AboutMeComponent } from './modules/presentation/components/about-me/about-me.component';
 
 const routes: Routes = [
   { path: '', component: PresentationComponent },
-  { path: 'about-me', component: PresentationComponent },
+  { path: 'about-me', component: AboutMeComponent },
   //  { path: 'projects', component: ProjectsComponent },
   //  { path: 'contact', component: ContactComponent },
   //  { path: '**', component: NotFoundComponent },
