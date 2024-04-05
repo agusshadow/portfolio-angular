c55a7bd2fd9ab41c882be15020e8e01d5fa7a178
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2024-04-05 00:52:35 -0300

    Adding icons using new library

--- src/app/modules/presentation/presentation.module.ts
+++ src/app/modules/presentation/presentation.module.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 import { PresentationComponent } from './components/presentation/presentation.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { TranslocoRootModule } from 'src/app/transloco-root.module';
+import { NgxBootstrapIconsModule, ColorTheme } from 'ngx-bootstrap-icons';
+import { linkedin, github } from 'ngx-bootstrap-icons';
+
+const icons = {
+  linkedin,
+  github
+};
 
 @NgModule({
   declarations: [
@@ -11,7 +18,12 @@ import { TranslocoRootModule } from 'src/app/transloco-root.module';
   ],
   imports: [
     CommonModule,
-    TranslocoRootModule
+    TranslocoRootModule,
+    NgxBootstrapIconsModule.pick(icons, {
+      width: '2em',
+      height: '2em',
+      theme: ColorTheme.Danger,
+  })
   ],
   exports: [
     PresentationComponent,
