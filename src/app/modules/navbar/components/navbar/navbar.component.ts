4567751aaf31aeabd514e3fa5a4d6d19a2396586
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2024-04-07 12:13:12 -0300

    New navbar design

--- src/app/modules/navbar/components/navbar/navbar.component.ts
+++ src/app/modules/navbar/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
-  constructor() { }
+  public switchValue: boolean = false;
+  public languaje: string = 'es';
+
+  constructor(private translocoService: TranslocoService) {}
+
+  switchToEnglish(): void {
+    this.translocoService.setActiveLang('en');
+  }
+
+  switchToSpanish(): void {
+    this.translocoService.setActiveLang('es');
+  }
+
+  switchLanguage(): void {
+    this.switchValue = !this.switchValue;
+    this.switchValue ? this.switchToEnglish() : this.switchToSpanish();
+  }
+
+  setLanguaje(language: string): void {
+    this.translocoService.setActiveLang(language);
+    this.languaje = this.translocoService.getActiveLang();
+  }
 
 }
